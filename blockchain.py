6c0a79ca62d94f91d9efc128970b6cf7c95f90bb
AravJuneja <aravjuneja@gmail.com>
2024-12-24 21:38:18 -0600

    Basic version of backend blockchain implemented.

---
+++ blockchain.py
@@ -0,0 +1,35 @@
+"""
+Author: Arav Juneja
+Github: aravjuneja
+"""
+
+import hashlib
+import time
+
+class Block:
+    def __init__(self, index, timestamp, data, previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.calculate_hash()
+
+    def calculate_hash(self):
+        data_to_hash = str(self.index) + str(self.timestamp) + str(self.data) + str(self.previous_hash)
+        return hashlib.sha256(data_to_hash.encode()).hexdigest()
+
+class Blockchain:
+    def __init__(self):
+        self.chain = [self.create_genesis_block()]
+
+    def create_genesis_block(self):
+        return Block(0, time.time(), "Genesis Block", "0")
+
+    def get_latest_block(self):
+        return self.chain[-1]
+
+    def add_block(self, data):
+        latest_block = self.get_latest_block()
+        new_block = Block(len(self.chain), time.time(), data, latest_block.hash)
+        self.chain.append(new_block)
+
